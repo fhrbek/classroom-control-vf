d4c3bdd4f87afbf07f4beb773aa4690f1bf42276
Filip Hrbek <filip.hrbek@gmail.com>
2016-02-23 14:31:29 +0100

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,40 @@
+class nginx {
+
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file { '/etc/nginx':
+    ensure => directory,
+    require => Package['nginx'],
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+
+  service { 'nginx':
+    ensure => running,
+    enable => false,
+    require => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
+  }
+}
