ee06cb112c7f58276b6f6ca1b948594fb53c1f68
Filip Hrbek <filip.hrbek@gmail.com>
2016-02-24 12:19:19 +0100

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,17 @@
+defined users::managed_user (
+  $username = $title,
+  $homedir = "/home/${username}",
+  $group = $username,
+  ) {
+
+  user { $title:
+    ensure => present,
+    managehome => false,
+    group => $group,
+  }
+  
+  file  { $homedir:
+    ensure => directory,
+    require => User[$user],
+  }
+}
